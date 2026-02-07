c18d3ea580c0cebdaf3ab577074336335c39df67
Holly Hong <holly.hong@mail.mcgill.ca>
2026-02-07 15:06:56 -0500

    rendered map

---
+++ my-app/app/main/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from 'react';
+import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+
+const Main = () => (
+    <div style={{ width: '100vw', height: '100vh' }}>
+        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string} 
+        onLoad={() => console.log('Maps API has loaded.')}>
+        <Map
+            defaultZoom={17}
+            defaultCenter={ { lat: 45.505074, lng: -73.577220 } }
+            onCameraChanged={ (ev: MapCameraChangedEvent) =>
+                console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+            }>
+        </Map>
+        </APIProvider>
+    </div>
+);
+
+export default Main;
